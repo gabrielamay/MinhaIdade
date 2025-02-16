7b051030d616a20fb18fea61e5475f6ac004c1ef
Gabriela May Canarin <gabi_may@hotmail.com>
2025-02-16 13:54:37 -0300

    Adiciona todos os arquivos

---
+++ app/src/main/java/br/com/fiap/minhaidade/MainActivity.kt
@@ -0,0 +1,113 @@
+package br.com.fiap.minhaidade
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+//import androidx.compose.ui.tooling.preview.Preview
+//import br.com.fiap.contador.ui.theme.ContadorTheme
+import br.com.fiap.minhaidade.ui.theme.MinhaIdadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MinhaIdadeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CounterScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CounterScreen() {
+    val idade = remember { mutableStateOf(0) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Qual a sua idade?",
+            fontSize = 24.sp,
+            color = Color(0xFFAD1F4E),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Aperte os botões para informar a sua idade.",
+            fontSize = 12.sp
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "${idade.value}",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row {
+            Button(
+                onClick = { if (idade.value > 0) idade.value-- },
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
+            ) {
+                Text(text = "-", fontSize = 40.sp)
+            }
+            Spacer(modifier = Modifier.width(32.dp))
+            Button(
+                onClick = {
+                    idade.value++
+                    Log.i("FIAP", "Idade: ${idade.value}")
+                },
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
+            ) {
+                Text(text = "+", fontSize = 40.sp)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = if (idade.value >= 18) "Você é MAIOR de idade!" else "Você é MENOR de idade!",
+            fontSize = 24.sp,
+            color = Color(0xFFAD1F4E),
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
